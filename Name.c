f77c975bf9ebb6bb248e613c79b40f4bffd53fe1
lawrencemwangi496-design <lawrencemwangi496@gmail.com>
2025-09-25 11:20:51 +0300

    Add files via upload

---
+++ Name.c
@@ -0,0 +1,15 @@
+// my name program
+#include <stdio.h>
+/* 
+Lawrence Gichuhi Mwangi 
+CT101/G/26436/25
+second code
+*/
+
+
+int main ()
+{
+//DISPLAY NAME 
+printf("Lawrence Mwangi");
+return 0;
+}
