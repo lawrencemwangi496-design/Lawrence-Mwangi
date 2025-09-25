da660be9bcf099c373e3b6d7bb3cd4180b369983
lawrencemwangi496-design <lawrencemwangi496@gmail.com>
2025-09-25 11:22:42 +0300

    Add files via upload

---
+++ Units.c
@@ -0,0 +1,18 @@
+/* 
+Lawrence Gichuhi mwangi 
+CT101/G/26436/25
+unit program
+*/
+#include<stdio.h>
+// display units
+int main(){
+printf("fundamental of ict\n");
+printf("introduction to programming and problem solving\n");
+printf("analogue electronics\n");
+printf("mathematics for science\n");
+printf("dicrete mathematics\n");
+printf("physics\n");
+printf("communication skills\n");
+printf("computer archtecture\n");
+return 0;
+}
