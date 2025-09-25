71a071036e2f74844d0f2ab44e84a8a0b78f4e97
lawrencemwangi496-design <lawrencemwangi496@gmail.com>
2025-09-25 11:21:23 +0300

    Add files via upload

---
+++ Age.c
@@ -0,0 +1,21 @@
+// my name program
+#include <stdio.h>
+/* 
+Lawrence Gichuhi Mwangi 
+CT101/G/26436/25
+Display age
+*/
+
+int main (){
+// variable declaration 
+int age;
+
+// prompt user enter data
+printf(" enter your age:");
+scanf("%d",&age);
+
+// display age
+printf("your age is %d",age);
+
+return 0;
+}
